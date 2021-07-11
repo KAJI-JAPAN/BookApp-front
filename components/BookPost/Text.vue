685b3ff770438a5e984627ee6348c9f46ec3e1ca
KAJI-JAPAN <kurowassan1206@gmail.com>
2021-07-12 07:28:00 +0900

    ユーザーの習慣化登録機能を追加

--- components/BookPost/Text.vue
+++ components/BookPost/Text.vue
@@ -18,97 +18,113 @@
         アクションを登録する
       </v-chip>
     </v-row>
-    <v-expand-transition>
-      <v-container
-        v-show="hidden"
-      >
-        <template #extension>
-          <v-fab-transition>
-            <v-btn
-              color="pink"
-              fab
-              dark
-              small
-              absolute
-              bottom
-              left
-            >
-              <v-icon>mdi-plus</v-icon>
-            </v-btn>
-          </v-fab-transition>
-        </template>
-        <v-row class="ma-2" justify="space-around">
-          <v-col class="pa-0">
-            <v-chip
-              class="ma-3"
-              color="red"
-              text-color="white"
-              icon="mdi-fire"
-            >
-              アクション
-              <v-icon>
-                mdi-fire
-              </v-icon>
-            </v-chip>
-          </v-col>
-          <v-col align-selef="end">
-            <UserKitHint />
-          </v-col>
-        </v-row>
-        <div
-          v-for="(text,index) in texts"
-          :key="index++"
-        >
-          <v-text-field
-            v-model="text['userText'+index]"
-            filled
-            :label="text.label"
-            :placeholder="text.placeholeder"
-            :color="text.color"
-            auto-grow
-          />
-        </div>
-        <div class="text-center">
+    <v-container
+      v-show="hidden"
+    >
+      <template #extension>
+        <v-fab-transition>
           <v-btn
+            color="pink"
+            fab
+            dark
             small
-            color="primary"
-            outlined
-            @click="hidden=!hidden"
+            absolute
+            bottom
+            left
           >
-            <v-icon dark>
-              mdi-minus
-            </v-icon>
+            <v-icon>mdi-plus</v-icon>
           </v-btn>
-        </div>
-      </v-container>
-    </v-expand-transition>
+        </v-fab-transition>
+      </template>
+      <v-row class="ma-2" justify="space-around">
+        <v-col class="pa-0">
+          <v-chip
+            class="ma-3"
+            color="red"
+            text-color="white"
+            icon="mdi-fire"
+          >
+            アクション
+            <v-icon>
+              mdi-fire
+            </v-icon>
+          </v-chip>
+        </v-col>
+        <v-col align-selef="end">
+          <UserKitHint />
+        </v-col>
+      </v-row>
+      <v-row v-for="(todo,index) in todos" :key="index">
+        <v-text-field
+          filled
+          readonly
+          :value="todo.text"
+        />
+        <v-btn @click="removeTodo(todo)">
+          削除
+        </v-btn>
+      </v-row>
+
+      <v-text-field
+        v-model="itemText"
+        filled
+        label="習慣化"
+        placeholder="(例) 運動は集中力を高めるので、いつもより10分早起きしてランニングをする"
+        color="pink lighten-3"
+        auto-grow
+        @keyup.enter="addTodo"
+      />
+      <v-btn
+        :disabled="disabled"
+        @click="addTodo"
+      >
+        保存
+      </v-btn>
+
+      <div class="text-center">
+        <v-btn
+          small
+          color="primary"
+          outlined
+          @click="hidden=!hidden"
+        >
+          <v-icon dark>
+            mdi-minus
+          </v-icon>
+        </v-btn>
+      </div>
+    </v-container>
   </v-container>
 </template>
 <script>
+import { mapMutations } from 'vuex'
 export default {
   data () {
     return {
       hidden: false,
-      texts: [
-        {
-          label: '習慣化1',
-          placeholeder: '(例) 運動は集中力を高めるので、いつもより10分早起きしてランニングをする',
-          color: 'pink lighten-3',
-          userText1: ''
+      itemText: ''
+    }
+  },
 
-        },
-        {
-          label: '習慣化2',
-          placeholeder: '(例) 毎朝7時から瞑想を行う',
-          color: 'pink lighten-3',
-          userText2: ''
+  computed: {
+    todos () {
+      return this.$store.state.todos.list
+    },
+    disabled () {
+      return this.itemText.length === 0
+    }
+  },
 
-        },
-        {
-          label: '習慣化3',
-          userText3: ''
-        }
-      ]
+  methods: {
+    addTodo () {
+      this.$store.commit('todos/add', this.itemText)
+      this.itemText = ''
+    },
+    ...mapMutations({
+      toggle: 'todos/toggle'
+    }),
+    removeTodo (todo) {
+      this.$store.commit('todos/remove', todo)
     }
   }
 }
