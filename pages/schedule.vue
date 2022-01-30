2c4e367ff96dabf7ef98f07f222fd55aa8f2fdad
KAJI-JAPAN <kurowassan1206@gmail.com>
2022-01-31 07:34:18 +0900

    add moment.js

---
+++ pages/schedule.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <v-sheet tile height="6vh" color="grey lighten-3" class="d-flex align-center">
+      <v-btn icon @click="$refs.calendar.prev()">
+        <v-icon>mdi-chevron-left</v-icon>
+      </v-btn>
+      <v-btn icon @click="$refs.calendar.next()">
+        <v-icon>mdi-chevron-right</v-icon>
+      </v-btn>
+    </v-sheet>
+    <v-sheet height="100vh">
+      <v-calendar
+        ref="calendar"
+        :events="events"
+        :event-color="getEventColor"
+        @change="getEvents"
+        v-model="value"
+      />
+    </v-sheet>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    events: []
+  }),
+  methods: {
+    getEvents () {
+      const events = [
+        {
+          name: '会議',
+          start: new Date('2022-01-22T01:00:00'), // 開始時刻
+          end: new Date('2022-01-22-T02:00:00'), // 終了時刻
+          color: 'blue',
+          timed: false // 終日ならfalse
+        }
+      ]
+      this.events = events
+    },
+    getEventColor (event) {
+      return event.color
+    }
+  }
+}
+</script>
--- test/store/smaple.spec.js
+++ test/store/smaple.spec.js
@@ -1,12 +1,12 @@
-// import axios from 'axios'
-// import Books from
+import axios from 'axios'
+import Books from
 
-// jest.mock('axios')
+jest.mock('axios')
 
-// test('', () => {
-//   const book = [{ title: 'test' }]
-//   const resp = [{ data: book }]
-//   axios.get.mockResolvedValue(resp)
+test('', () => {
+  const book = [{ title: 'test' }]
+  const resp = [{ data: book }]
+  axios.get.mockResolvedValue(resp)
 
-//   return Books.all().then(data => expect(data).toEqual(book))
-// })
+  return Books.all().then(data => expect(data).toEqual(book))
+})
