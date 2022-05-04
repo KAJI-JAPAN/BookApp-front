7e94987fb16e1b4fae5f7b0bfd034a70bcfedca4
KAJI-JAPAN <kurowassan1206@gmail.com>
2022-05-04 20:26:08 +0900

    ページネーション追加

--- components/RegisteredBook/Main.vue
+++ components/RegisteredBook/Main.vue
@@ -1,7 +1,14 @@
 <template>
   <v-container class="back">
+    <v-pagination
+      v-if="lists.length !==0"
+      v-model="page"
+      :length="length"
+      circle
+      @input="pageChange"
+    />
     <div
-      v-if="registeredBooks.length === 0"
+      v-if="lists.length === 0"
       class="text-center"
     >
       <p class="pa-13">
@@ -11,7 +18,7 @@
     <div v-else>
       <v-row class="ma-3">
         <v-col
-          v-for="(book, index) in registeredBooks"
+          v-for="(book, index) in displayLists"
           :key="index"
           cols="12"
           class="ma-1"
@@ -37,25 +44,44 @@
         </v-col>
       </v-row>
     </div>
+    <v-pagination
+      v-if="lists.length !==0"
+      v-model="page"
+      :length="length"
+      circle
+      @input="pageChange"
+    />
   </v-container>
 </template>
 <script>
 import * as url from '@/store/constants/url'
 export default {
-
-  // storeから登録した書籍情報の配列を取得
-  computed: {
-    registeredBooks () {
-      return this.$store.state.book.registeredBook
+  data () {
+    return {
+      page: 1,
+      length: 0,
+      lists: [],
+      displayLists: [],
+      pageSize: 10
     }
   },
 
   // 登録した書籍情報を取得
+  // ページネーションを設定
   mounted () {
     this.$axios.$get(url.POST_API + 'posts')
       .then((response) => {
-        this.$store.commit('book/registeredBook', response)
+        this.lists = response
+        this.length = Math.ceil(this.lists.length / this.pageSize)
+        this.displayLists = this.lists.slice(0, this.pageSize)
       })
+  },
+
+  // ページネーションクリック時に要素を取得
+  methods: {
+    pageChange (pageNumber) {
+      this.displayLists = this.lists.slice(this.pageSize * (pageNumber - 1), this.pageSize * (pageNumber))
+    }
   }
 }
 </script>
