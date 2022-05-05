2ffe7bb061944e91e544671d1652e5300bd29875
KAJI-JAPAN <kurowassan1206@gmail.com>
2022-05-05 15:05:37 +0900

    使い方ページ更新

--- pages/help.vue
+++ pages/help.vue
@@ -1,34 +1,67 @@
 <template>
   <v-container class="back">
-    <div align="center">
-      <h1 class="pa-4">
-        本で得た知識を習慣に落とし込もう
-      </h1>
-      <p>KOUDOKUでは本から得た知識を行動に移すためのサポートを行います。</p>
-      <p>本を読んで終わるのではなく、知識を活かしてより豊かな人生を歩みましょう</p>
-    </div>
-    <v-divider class="pa-3" />
-    <div
-      v-for="(item, i) in helpItem"
-      :key="i"
-    >
-      <h2>{{ item.title }}</h2>
-      <p>{{ item.text }}</p>
-      <v-img
-        :src="require(`~/assets/images/${item.image}`)"
-        alt="help"
-      />
-    </div>
-    <!-- <div> -->
-    <!-- <h2>読んだ本を管理する</h2>
-      <p>読んだ本は一覧で管理することができます。</p>
-      <v-img src="" />
-    </div>
-    <div>
-      <h2>読んだ本から得た知識を習慣化する</h2>
-      <p>本から得た知識を習慣化するためのサポートをします。</p>
-      <v-img src="" /> -->
-    <!-- </div> -->
+    <v-card class="pa-6 ma-3">
+      <div align="center">
+        <h1 class="pa-4">
+          アプリの使い方
+        </h1>
+      </div>
+      <h2>1.読んだ本を探す</h2>
+      <v-divider class="pa-3" />
+      <div align="start">
+        <v-card-text>
+          <p>検索ボックスから読んだ本を探して、その後のアクションを入力しましょうアクションは出来るだけ具体的に、少なくしましょう。</p>
+          <p>普段の生活をほんの少しだけ変えるぐらいがおすすめです。</p>
+        </v-card-text>
+        <div align="center">
+          <video controls width="800">
+
+            <source
+              src="~/assets/images/search.mp4"
+              type="video/webm"
+            >
+
+            <source
+              src="~/assets/images/search.mp4"
+              type="video/mp4"
+            >
+
+            Sorry, your browser doesn't support embedded videos.
+          </video>
+        </div>
+      </div>
+      <div>
+        <h2 class="mt-15">
+          2.読んだ本を管理する
+        </h2>
+        <v-divider class="pa-3" />
+        <v-card-text>自分がどんな本を読んだか一目で確認できます。</v-card-text>
+        <v-img :src="require(`~/assets/images/book-list.png`)" />
+      </div>
+      <div>
+        <h2 class="mt-15">
+          3.読んだ本から得た知識を習慣化する
+        </h2>
+        <v-divider class="pa-3" />
+        <v-card-text>本から得た知識を習慣化するためのサポートをします。いつ行うかカレンダーに記載して習慣化を目指しましょう！</v-card-text>
+        <div align="center">
+          <video controls width="800">
+
+            <source
+              src="~/assets/images/schedule.mp4"
+              type="video/webm"
+            >
+
+            <source
+              src="~/assets/images/schedule.mp4"
+              type="video/mp4"
+            >
+
+            Sorry, your browser doesn't support embedded videos.
+          </video>
+        </div>
+      </div>
+    </v-card>
   </v-container>
 </template>
 <script>
@@ -36,13 +69,7 @@ export default {
   auth: false,
   data ({ $config: { APP_NAME } }) {
     return {
-      APP_NAME,
-      helpItem: [
-        // { title: '読んだ本を探す', text: '検索ボックスから読んだ本を検索しましょう。', image: '//' },
-        { title: '読んだ本を管理する', text: '読んだ本は一覧で管理することができます。', image: 'book-list.png' }
-        // { title: '読んだ本から得た知識を習慣化する', text: '本から得た知識を習慣化するためのサポートをします。', image: '//' }
-
-      ]
+      APP_NAME
     }
   }
 }
